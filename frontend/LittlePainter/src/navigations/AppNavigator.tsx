39b1a45db7042d1e694c4e1c0ba4c8bef882f7c2
kimbeomgi <dyd1308@naver.com>
2023-10-19 17:54:38 +0900

    #S09P31D106-79 #done #feat(main) : 초기 메인화면 목업 구성완료

--- frontend/LittlePainter/src/navigations/AppNavigator.tsx
+++ frontend/LittlePainter/src/navigations/AppNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import MainScreen from '../screens/main/MainScreen';
+import MainTmpScreen from '../screens/main/MainTmpScreen';
 import Detail1Screen from '../screens/detail/Detail1Screen';
 import DetailScreen from '../screens/detail/DetailScreen';
 import NodetailScreen from '../screens/detail/NodetailScreen';
@@ -8,6 +9,7 @@ import Detail2Screen from '../screens/detail/Detail2Screen';
 
 export type RootStackParams = {
   MainScreen: undefined;
+  MainTmpScreen: undefined;
   DetailScreen: undefined;
   NodetailScreen: undefined;
   Detail1Screen: {
@@ -21,8 +23,11 @@ const RootStack = createStackNavigator<RootStackParams>();
 
 export default function AppNavigator() {
   return (
-    <RootStack.Navigator initialRouteName="MainScreen">
+    <RootStack.Navigator
+      initialRouteName="MainScreen"
+      screenOptions={{headerShown: false}}>
       <RootStack.Screen name="MainScreen" component={MainScreen} />
+      <RootStack.Screen name="MainTmpScreen" component={MainTmpScreen} />
       <RootStack.Screen name="DetailScreen" component={DetailScreen} />
       <RootStack.Screen name="Detail1Screen" component={Detail1Screen} />
       <RootStack.Screen name="Detail2Screen" component={Detail2Screen} />
