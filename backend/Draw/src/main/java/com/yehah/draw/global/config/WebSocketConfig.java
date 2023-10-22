1006590497bf56cfb858e03f80ab9cc3d755ef22
Poposong <psi1908@naver.com>
2023-10-23 00:02:24 +0900

    #S09P31D106-40 #in-progress #feat(config) : Draw 서비스 웹소켓 통신 확인

--- backend/Draw/src/main/java/com/yehah/draw/global/config/WebSocketConfig.java
+++ backend/Draw/src/main/java/com/yehah/draw/global/config/WebSocketConfig.java
@@ -1,4 +1,25 @@
 package com.yehah.draw.global.config;
 
-public class WebSocketConfig{
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(signalWebSocketHandler(), "/room")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public WebSocketHandler signalWebSocketHandler(){
+        return new WebSocketHandler();
+    }
 }
---
+++ backend/Draw/src/main/java/com/yehah/draw/global/config/WebSocketHandler.java
@@ -0,0 +1,35 @@
+package com.yehah.draw.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception{
+        var sessionId = session.getId();
+        sessions.put(sessionId, session);
+
+        Message message = Message.builder().sender(sessionId).receiver("all").build();
+        message.newConnect();
+
+        sessions.values().forEach(s -> {
+            try{
+                if(!s.getId().equals(sessionId)){
+                    s.sendMessage(new TextMessage(Utils.getString(message)));
+                }
+            }catch(Exception e){
+                // TODO : throw
+            }
+        });
+
+    }
+
+}
