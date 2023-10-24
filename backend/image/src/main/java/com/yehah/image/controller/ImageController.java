96ec9dbd0cd49aed4cb9c4793aee8d1aad022731
김소이 <thdl9893@ajou.ac.kr>
2023-10-24 17:27:32 +0900

    #{nojiraissue} #done #fix(Image) : url 변경 완료

--- backend/image/src/main/java/com/yehah/image/controller/ImageController.java
+++ backend/image/src/main/java/com/yehah/image/controller/ImageController.java
@@ -32,14 +32,14 @@ public class ImageController {
 	private final ImageService imageService;
 
 	@Operation(summary = "S3- 마이페이지에 그림 저장하기", description = "(draw -> image) aws s3에 마이페이지에 저장할 그림을 저장한다.")
-	@PostMapping(value = "/comm-myWork", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/myWork", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SaveImageResponseDto> uploadMyPage(@ModelAttribute SaveImageRequestDto saveImageRequestDto) throws IOException {
 		log.debug("uploadMyPage()");
 		return ResponseEntity.status(201).body(imageService.uploadMyPage(saveImageRequestDto));
 	}
 
 	@Operation(summary = "S3- 내 동물 사진 올리기", description = "(draw -> image) aws s3에 내 동물 사진 올리기에 업로드할 사진을 저장한다.")
-	@PostMapping(value = "/comm-myAnimal", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/myAnimal", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SaveMyAnimalResponseDto> uploadMyAnimalImage(@ModelAttribute SaveMyAnimalRequestDto saveMyAnimalRequestDto) throws IOException {
 		log.debug("uploadMyAnimalImage()");
 		return ResponseEntity.status(201).body(imageService.uploadMyAnimalImage(saveMyAnimalRequestDto));
