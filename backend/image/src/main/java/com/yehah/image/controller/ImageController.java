058a922965fd2d95fe63bb6fe7ca6e550e28dec7
김소이 <thdl9893@ajou.ac.kr>
2023-10-24 16:08:44 +0900

    #S09P31D106-112 #done #feat(Image) : swagger 적용

--- backend/image/src/main/java/com/yehah/image/controller/ImageController.java
+++ backend/image/src/main/java/com/yehah/image/controller/ImageController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,23 +17,29 @@ import com.yehah.image.dto.SaveMyAnimalRequestDto;
 import com.yehah.image.dto.SaveMyAnimalResponseDto;
 import com.yehah.image.service.ImageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Tag(name = "Image Controller", description = "Image Controller의 API입니다.")
 @RestController
 @RequestMapping("/api/v1/images")
 @RequiredArgsConstructor
 public class ImageController {
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	private final ImageService imageService;
 
-	@PostMapping(name = "S3- 마이페이지에 그림 저장하기", value = "/comm-myWork")
+	@Operation(summary = "S3- 마이페이지에 그림 저장하기", description = "(draw -> image) aws s3에 마이페이지에 저장할 그림을 저장한다.")
+	@PostMapping(value = "/comm-myWork", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SaveImageResponseDto> uploadMyPage(@ModelAttribute SaveImageRequestDto saveImageRequestDto) throws IOException {
 		log.debug("uploadMyPage()");
 		return ResponseEntity.status(201).body(imageService.uploadMyPage(saveImageRequestDto));
 	}
 
-	@PostMapping(name = "S3- 내 동물 사진 올리기", value = "/comm-myAnimal")
+	@Operation(summary = "S3- 내 동물 사진 올리기", description = "(draw -> image) aws s3에 내 동물 사진 올리기에 업로드할 사진을 저장한다.")
+	@PostMapping(value = "/comm-myAnimal", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SaveMyAnimalResponseDto> uploadMyAnimalImage(@ModelAttribute SaveMyAnimalRequestDto saveMyAnimalRequestDto) throws IOException {
 		log.debug("uploadMyAnimalImage()");
 		return ResponseEntity.status(201).body(imageService.uploadMyAnimalImage(saveMyAnimalRequestDto));
