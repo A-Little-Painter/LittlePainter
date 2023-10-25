8a55cea00183f34ad0bac12d4c61737a9fb22379
김소이 <thdl9893@ajou.ac.kr>
2023-10-25 21:49:17 +0900

    #S09P31D106-227 #in-progress #feat(entity) : draw 서비스의 entity 생성

---
+++ backend/Draw/src/main/java/com/yehah/draw/domain/animals/entity/AnimalType.java
@@ -0,0 +1,30 @@
+package com.yehah.draw.domain.animals.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "animal_type")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalType {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", length = 20, nullable = false)
+	private String name;
+
+	@Column(name = "url_sound", length = 200, nullable = false)
+	private String urlSound;
+}
