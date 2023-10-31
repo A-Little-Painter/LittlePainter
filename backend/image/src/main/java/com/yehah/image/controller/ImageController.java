0e6c60dd73ba573937cd39a6cd1e41a56869e4dd
Poposong <psi1908@naver.com>
2023-10-31 17:35:34 +0900

    #S09P31D106-352 #done #feat(animal): image서버 호출해서 내가 그린 최종 그림 S3에 저장하기

--- backend/image/src/main/java/com/yehah/image/controller/ImageController.java
+++ backend/image/src/main/java/com/yehah/image/controller/ImageController.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.yehah.image.dto.request.SaveImageRequest;
 import com.yehah.image.dto.response.SaveImageResponse;
@@ -19,6 +16,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Tag(name = "Image Controller", description = "Image Controller의 API입니다.")
@@ -30,12 +28,14 @@ public class ImageController {
 	private final ImageService imageService;
 
 	@Operation(summary = "S3- 마이페이지에 그림 저장하기", description = "(draw -> image) aws s3에 마이페이지에 저장할 그림을 저장한다.")
-	@PostMapping(value = "/myWork", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<SaveImageResponse> uploadMyPage(@ModelAttribute SaveImageRequest saveImageRequest) throws IOException {
-		log.debug("uploadMyPage()");
+	@PostMapping(value = "/myWork")
+	public ResponseEntity<SaveImageResponse> uploadMyPage(@RequestParam Long userId,@RequestParam String category,@RequestParam MultipartFile image) throws IOException {
+		log.info(category);
+		SaveImageRequest saveImageRequest = SaveImageRequest.builder()
+						.userId(userId).category(category).image(image).build();
 		return ResponseEntity.status(201).body(imageService.uploadMyPage(saveImageRequest));
 	}
-
+// , consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
 	@Operation(summary = "S3- 내 동물 사진 올리기", description = "(draw -> image) aws s3에 내 동물 사진 올리기에 업로드할 사진을 저장한다.")
 	@PostMapping(value = "/myAnimal", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SaveMyAnimalResponse> uploadMyAnimalImage(@ModelAttribute SaveMyAnimalRequest saveMyAnimalRequest) throws IOException {
