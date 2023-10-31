109991053495dd786b27067d625f960d001620e5
김소이 <thdl9893@ajou.ac.kr>
2023-10-31 15:32:26 +0900

    #S09P31D106 #in-progress #feat(animal_type) : 전체 동물 종류 목록 조회

---
+++ backend/Draw/src/main/java/com/yehah/draw/domain/animal_type/dto/response/AnimalTypeListResDto.java
@@ -0,0 +1,6 @@
+package com.yehah.draw.domain.animal_type.dto.response;
+
+public interface AnimalTypeListResDto {
+	Long getId();
+	String getName();
+}
--- backend/Draw/src/main/java/com/yehah/draw/domain/animal_type/service/AnimalTypeService.java
+++ backend/Draw/src/main/java/com/yehah/draw/domain/animal_type/service/AnimalTypeService.java
@@ -1,4 +1,9 @@
 package com.yehah.draw.domain.animal_type.service;
 
+import java.util.List;
+
+import com.yehah.draw.domain.animal_type.dto.response.AnimalTypeListResDto;
+
 public interface AnimalTypeService {
+	public List<AnimalTypeListResDto> getAnimalTypeList();
 }
