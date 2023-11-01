b8c2dcf6a782eb072e42930dfc1d251c7bb91c28
정예진 <psi1908@naver.com>
2023-11-01 13:26:13 +0900

    Update backend/SimilarCheck/app.py

--- backend/SimilarCheck/app.py
+++ backend/SimilarCheck/app.py
@@ -54,6 +54,6 @@ def SimilarityCheck():
 #     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=8600, debug=True)
+    app.run('0.0.0.0', port=8600, debug=True)
 
 
