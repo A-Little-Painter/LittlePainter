796a8b2be34150b6728112bef519f3a52721372f
Ubuntu <ubuntu@ip-172-26-14-115.ap-northeast-2.compute.internal>
2023-11-02 08:32:17 +0000

    #S09P31D106-269 #in-progress #setting(Animate) : 플라스크 서버 초기 세팅
    Former-commit-id: 4650d44a656444faec56a6a26ffa698ba535fbb8

--- backend/Animate/app.py
+++ backend/Animate/app.py
@@ -1,12 +1,43 @@
 # app.py
+import logging
+
 from flask import Flask
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    print("Hello D106")
+@api.route('/hello')
+class Hello(Resource):
+    def get(self):
+        key = "hello"
+        value = "d106!"
+        return {
+            key: value
+        }
+
+
+@app.route('/draws/animations/animals')
+class AnimateAnimal(Resource):
+    def post(self, image, animalType):
+        logging.debug("Animate-Service : animateAnimal Called")
+        return {
+            image: image,
+            animalType: animalType
+        }
+
+
+@app.route('/draws/animations/tales')
+class AnimateCharacter(Resource):
+    def post(self, image, title, pageno):
+        logging.debug("Animate-Service : animateCharacter Called")
+        return {
+            image: image
+            , title: title
+            , pageno: pageno
+            # ,characterName : characterName
+        }
 
 
 if __name__ == "__main__":
