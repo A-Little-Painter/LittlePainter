4535cc73d3e74291748e506b90ecf1738910d544
방진성 <xkakrlfh@naver.com>
2023-11-04 16:34:21 +0900

    #S09P31D106-334 #done #feat(Auth) : auth, gateway complete 일단 주석해놓겠음

---
+++ backend/Draw/src/main/java/com/yehah/draw/domain/user/response/ChildResponseDTO.java
@@ -0,0 +1,12 @@
+package com.yehah.draw.domain.user.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChildResponseDTO {
+    private Long userId;
+    private Long childId;
+    private String role;
+}
