99ec46bbaec471464d99708e552f57802800b739
ec2-animate server <ubuntu@ip-172-26-14-115.ap-northeast-2.compute.internal>
2023-11-05 16:30:22 +0000

    #S09P31D106 #in-progress #feature(Animate) : body수신, gif반환 구조 생성
    Former-commit-id: 02eee9423e4af27278c320b8513870124fc4b6d7

--- backend/Animate/app.py
+++ backend/Animate/app.py
@@ -1,8 +1,9 @@
 # app.py
 import logging
 
-from flask import Flask
+from flask import Flask, request, send_file
 from flask_restx import Api, Resource
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,33 +13,53 @@ api = Api(app)
 class Hello(Resource):
     def get(self):
         key = "hello"
-        value = "d106!"
+        value = "d106!@##"
         return {
             key: value
-        }
+        }, 201, None
 
 
-@app.route('/draws/animations/animals')
-class AnimateAnimal(Resource):
-    def post(self, image, animalType):
+@api.route('/draws/animations/animals')
+class TodoSimple(Resource):
+    def post(self):
+        # 진입 확인
         logging.debug("Animate-Service : animateAnimal Called")
-        return {
-            image: image,
-            animalType: animalType
-        }
+
+        # requestbody 수신
+        animal_type = request.form['animalType']
+        image = request.files['image']
+
+        # 이미지 저장
+        filename = secure_filename(image.filename)
+        image.save(filename)
+
+        # 저장한 이미지로 애니메이션 생성
+        # todo : cli실행
+
+        # 임시값 반환
+        return send_file("examples/result/rabiit/video.gif", mimetype='image/gif')
 
 
-@app.route('/draws/animations/tales')
+@api.route('/draws/animations/tales')
 class AnimateCharacter(Resource):
-    def post(self, image, title, pageno):
-        logging.debug("Animate-Service : animateCharacter Called")
-        return {
-            image: image
-            , title: title
-            , pageno: pageno
-            # ,characterName : characterName
-        }
+    def post(self):
+        # requestbody 수신
+        animal_type = request.form['animalType']
+        pageNo = request.form['pageNo']
+        image = request.files['image']
+
+        # 이미지 저장
+        filename = secure_filename(image.filename)
+        image.save(filename)
+
+        # 저장한 이미지로 애니메이션 생성
+        # todo : cli실행
+
+        # 임시애니메이션 반환
+        return send_file("examples/result/rabiit/video.gif", mimetype='image/gif')
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     app.run('0.0.0.0', port=8700, debug=True)
