567865d67d334031bcaef61fcb64cb9fb33462bd
Poposong <psi1908@naver.com>
2023-11-06 22:01:24 +0900

    #S09P31D106-353 #in-progress #feat(animals): StompService 분리, 에러 추가, AnimalController 코드 정리

--- backend/Draw/src/main/java/com/yehah/draw/domain/animal/controller/AnimalControllerAdvice.java
+++ backend/Draw/src/main/java/com/yehah/draw/domain/animal/controller/AnimalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.yehah.draw.domain.animal.controller;
 
+import com.yehah.draw.domain.animal.exception.SaveImageException;
 import com.yehah.draw.domain.animal.exception.SimilarityCheckException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,12 @@ public class AnimalControllerAdvice {
     // NOTE : 유사도 검사 측정에 실패했습니다.
     @ExceptionHandler
     public ResponseEntity<?> handleSimilarityCheckException(SimilarityCheckException e){
+        return ResponseEntity.status(404).body(e.getMessage());
+    }
+
+    // NOTE : 이미지를 S3에 저장할 수 없습니다.
+    @ExceptionHandler
+    public ResponseEntity<?> handlerSaveImageException(SaveImageException e){
         return ResponseEntity.status(404).body(e.getMessage());
     }
 }
---
+++ backend/Draw/src/main/java/com/yehah/draw/global/stomp/StompService.java
@@ -0,0 +1,26 @@
+package com.yehah.draw.global.stomp;
+
+import com.yehah.draw.domain.animal.entity.SimilarState;
+import com.yehah.draw.global.stomp.dto.MessageResponse;
+import com.yehah.draw.global.stomp.dto.SimilarMessageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StompService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    // NOTE : STOMP 성공 응답 전송하기
+    public void AnimalSuccessRes(String responseUrl, SimilarState similarState, SimilarMessageResponse similarMessageResponse){
+        similarMessageResponse.setSimilarState(SimilarState.END);
+        messagingTemplate.convertAndSend(responseUrl, similarMessageResponse);
+    }
+
+    // NOTE : STOMP 실패 응답 전송하기
+    public void AnimalFailRes(String responseUrl, MessageResponse messageResponse){
+        messagingTemplate.convertAndSend(responseUrl, messageResponse);
+    }
+}
