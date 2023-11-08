f4ab1ef63e990cd7f58084dfaa841591b3478eb5
방진성 <xkakrlfh@naver.com>
2023-11-08 16:57:27 +0900

    #S09P31D106-334 #done #fix(User) : @Repository

--- backend/user/src/main/java/com/yehah/user/domain/user/repository/ChildRepository.java
+++ backend/user/src/main/java/com/yehah/user/domain/user/repository/ChildRepository.java
@@ -2,6 +2,8 @@ package com.yehah.user.domain.user.repository;
 
 import com.yehah.user.domain.userAuth.entity.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
 }
