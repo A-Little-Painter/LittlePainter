1b977fdcd699b8953c16254f495ba97541038ca8
방진성 <xkakrlfh@naver.com>
2023-11-10 10:20:01 +0900

    #S09P31D106-334 #done #fix(User) : 아이콘 변경

--- backend/user/src/main/java/com/yehah/user/domain/user/service/UserServiceImpl.java
+++ backend/user/src/main/java/com/yehah/user/domain/user/service/UserServiceImpl.java
@@ -155,23 +155,19 @@ public class UserServiceImpl implements UserService{
     }
 
     public ResponseEntity<?>  changeIcon(ChangeIconRequestDTO changeIconRequestDTO){
-        User user = getLoginUser();
-        log.info("user.getEmail() "+user.getEmail());
+
         Icon icon = iconRepository.findById(changeIconRequestDTO.getIconId())
                 .orElseThrow(() -> new IllegalArgumentException("아이콘을 찾을 수 없습니다."));
-        return userRepository.findById(user.getId())
-                .map(userFromDB -> {
-                    Child selectedChild = userFromDB.getChildren().stream()
-                            .filter(child -> child.getId().equals(userFromDB.getLastSelectedChildId()))
-                            .findFirst().orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이입니다."));
-                    try{
-                        childRepository.save(selectedChild.updateIcon(icon));
-                    }catch (Exception e){
-                        throw new DatabaseException("DB에 저장할 수 없습니다.");
-                    }
-                    return ResponseEntity.ok().body("아이콘 변경 성공");
-                })
-                .orElseThrow(() -> new UserNotFoundException("로그인 사용자를 찾을 수 없습니다."));
+        Child child = childRepository.findById(changeIconRequestDTO.getChildId())
+                .orElseThrow(() -> new IllegalArgumentException("아이를 찾을 수 없습니다."));
+        
+        try{
+            childRepository.save(child.updateIcon(icon));
+        } catch (Exception e){
+            throw new DatabaseException("DB 접근 불가능");
+        }
+
+        return ResponseEntity.ok().body("아이콘 변경 성공");
     }
 
     public ResponseEntity<?> updateChild(ChangeChildRequestDTO changeChildRequestDTO){
