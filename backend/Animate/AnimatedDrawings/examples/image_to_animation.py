1c705e129290d77a1a4a3ebe26ae625326abbee4
ec2-animate server <eorms96@naver.com>
2023-11-10 03:58:17 +0000

    #S09P31D106-430 #in-progress #fix(Animate) : 카테고리별 애니메이션 분류 디버깅(3) - 로거 수정
    Former-commit-id: 49ed0c3a578cb463159b9205f80b10da2ca36782

--- backend/Animate/AnimatedDrawings/examples/image_to_animation.py
+++ backend/Animate/AnimatedDrawings/examples/image_to_animation.py
@@ -26,31 +26,34 @@ if __name__ == '__main__':
     log_dir = Path('./logs')
     log_dir.mkdir(exist_ok=True, parents=True)
     logging.basicConfig(filename=f'{log_dir}/log.txt', level=logging.DEBUG)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
     # 인자 수신
     img_fn = sys.argv[1]
     char_anno_dir = sys.argv[2]
 
-    if len(sys.argv) > 3: # animal, tale 완료하면 조건 제거
+    if len(sys.argv) > 3: # animal, tale 완료하면 조건 제거(1)
         character = sys.argv[3]
         animation_type = sys.argv[4]
-        logging.debug('기본인자 : ' + img_fn + ' / ' + char_anno_dir + ' / ' + character + ' / ' + animation_type)
-    else: # animal, tale 완료하면 제거
+        logger.debug('기본인자 : ' + img_fn + ' / ' + char_anno_dir + ' / ' + character + ' / ' + animation_type)
+    else: # animal, tale 완료하면 내용까지 제거(2)
         motion_cfg_fn = resource_filename(__name__, 'config/motion/dab.yaml')
         retarget_cfg_fn = resource_filename(__name__, 'config/retarget/fair1_ppf.yaml')
         image_to_animation(img_fn, char_anno_dir, motion_cfg_fn, retarget_cfg_fn)
 
-    animation_type = None #나중에 제거
+    animation_type = None #나중에 제거(3)
 
     if animation_type == 'animals':
         animal_list = ['강아지', '고양이', '코끼리', '소', '오리', '쥐', '사자', '닭', '늑대', '원숭이', '돼지', '호랑이', '곰', '기타']
+        four_legs_animal = ['강아지', '고양이', '코끼리', '']
         motion_cfg_fn = resource_filename(__name__, 'config/motion/zombie.yaml')
         retarget_cfg_fn = resource_filename(__name__, 'config/retarget/four_legs.yaml')
         image_to_animation(img_fn, char_anno_dir, motion_cfg_fn, retarget_cfg_fn)
     elif animation_type == 'tales':
         title = sys.argv[5]
         page_no = sys.argv[6]
-        logging.debug('동화 추가 인자 : ' + title + ' / ' + page_no)
+        logger.debug('동화 추가 인자 : ' + title + ' / ' + page_no)
         motion_cfg_fn = resource_filename(__name__, 'config/motion/dab.yaml')
         retarget_cfg_fn = resource_filename(__name__, 'config/retarget/fair1_ppf.yaml')
         image_to_animation(img_fn, char_anno_dir, motion_cfg_fn, retarget_cfg_fn)
