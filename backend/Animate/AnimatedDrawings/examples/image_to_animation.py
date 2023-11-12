cfdccde1af7b972f85505fb4c464585b978b6633
ec2-animate server <eorms96@naver.com>
2023-11-12 07:26:08 +0000

    #S09P31D106 #in-progress #feat(Animate) : 생성된 char_cfg를 프리셋으로 대체 - 임시 롤백
    Former-commit-id: 0d88f04e91aff62c0607ed8786ff16c0f8a1b755

--- backend/Animate/AnimatedDrawings/examples/image_to_animation.py
+++ backend/Animate/AnimatedDrawings/examples/image_to_animation.py
@@ -3,9 +3,12 @@
 # LICENSE file in the root directory of this source tree.
 import shutil
 
+import yaml
+
 from image_to_annotations import image_to_annotations
 from annotations_to_animation import annotations_to_animation
 from pathlib import Path
+from PIL import Image, ImageOps
 import logging
 import sys
 from pkg_resources import resource_filename
@@ -21,20 +24,29 @@ def image_to_animation(img_fn: str, char_anno_dir: str, motion_cfg_fn: str, reta
     # create the annotations
     image_to_annotations(img_fn, char_anno_dir, animation_type)
 
-    # 모델이 도출한 annotation(mask, texture) 프리셋에 맞게 수정
-    # char_cfg파일 복사
-    if animation_type == 'tales' or animation_type == 'animals':
-        try:
-            # Copy the file to the destination folder
-            shutil.copy(f"{char_anno_dir}/../char_cfg.yaml", f"{char_anno_dir}/char_cfg.yaml")
-            print("File copied successfully.")
-        except FileNotFoundError:
-            print("Source file not found.")
-        except PermissionError:
-            print("Permission error: Check if you have write access to the destination folder.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-    # 복사한 char_cfg파일에 맞게 이미지 수정
+    # # 모델이 도출한 annotation(mask, texture) 프리셋에 맞게 수정
+    # # char_cfg파일 복사
+    # if animation_type == 'tales' or animation_type == 'animals':
+    #     try:
+    #         # Copy the file to the destination folder
+    #         shutil.copy(f"{char_anno_dir}/../char_cfg.yaml", f"{char_anno_dir}/char_cfg.yaml")
+    #         print("File copied successfully.")
+    #     except FileNotFoundError:
+    #         print("Source file not found.")
+    #     except PermissionError:
+    #         print("Permission error: Check if you have write access to the destination folder.")
+    #     except Exception as e:
+    #         print(f"An error occurred: {e}")
+    # # 복사한 char_cfg파일에 맞게 이미지 수정
+    # # # cfg파일 로드
+    # with open(f"{char_anno_dir}/char_cfg.yaml", "r") as file:
+    #     cfg_file = yaml.safe_load(file)
+    # # # image파일 로드, 수정, 저장
+    # texture = Image.open(f"{char_anno_dir}/texture.png")
+    # resized_image = ImageOps.pad(texture, (cfg_file["width"], cfg_file["height"]), method=0, color=(255, 255, 255))
+    #
+    # mask = Image.open(f"{char_anno_dir}/mask.png")
+    # cfg_file["width"] = 10
 
     # create the animation
     annotations_to_animation(char_anno_dir, motion_cfg_fn, retarget_cfg_fn, animation_type, character)
@@ -48,14 +60,12 @@ if __name__ == '__main__':
     animation_type = None  # 나중에 제거(3)
     character = None
 
-    logging.debug(f"image_to_animation.py 진입 {len(sys.argv)}")
     img_fn = sys.argv[1]
     char_anno_dir = sys.argv[2]
 
     if len(sys.argv) > 3:  # animal, tale 완료하면 조건 제거(1)
         character = sys.argv[3]
         animation_type = sys.argv[4]
-        logging.debug('기본인자 : ' + img_fn + ' / ' + char_anno_dir + ' / ' + character + ' / ' + animation_type)
     else:  # animal, tale 완료하면 내용까지 제거(2)
         motion_cfg_fn = resource_filename(__name__, 'config/motion/dab.yaml')
         retarget_cfg_fn = resource_filename(__name__, 'config/retarget/fair1_ppf.yaml')
