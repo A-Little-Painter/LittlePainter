69481f27ba95c67197a8e692a6e881adb4000d74
이대근 <eorms96@naver.com>
2023-11-21 16:38:55 +0900

    #S09P31D106 #done #fix(Draw) : zip파일 해제 후 정렬 수정(4)

--- backend/Draw/src/main/java/com/yehah/draw/global/common/ImageListToRequestBodyData.java
+++ backend/Draw/src/main/java/com/yehah/draw/global/common/ImageListToRequestBodyData.java
@@ -1,13 +1,20 @@
 package com.yehah.draw.global.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.util.MultiValueMap;
 
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class ImageListToRequestBodyData {
     public static void addImage(Map<String, byte[]> images, MultiValueMap<String, Object> bodyData) {
+        log.error("images 사이즈 = " + images.size());
+        if(images.size() < 2) {
+            System.out.println("images 사이즈 2보다 작음" + images.size());
+            log.error("images 사이즈 2보다 작음" + images.size());
+        }
         bodyData.add("image_mask", new ByteArrayResource(images.get("output_mask.jpg")){
             @Override
             public String getFilename() throws IllegalStateException{
