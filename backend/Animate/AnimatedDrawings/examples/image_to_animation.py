aca2d2ba09a438bf80f0863fb9db3759c4a81296
ec2-animate server <eorms96@naver.com>
2023-11-21 15:52:43 +0000

    #S09P31D106 #done #fix(Animate) : 동물그리기만 새 방식으로 적용
    Former-commit-id: 5296b66b48ff9d2aa1fbab2c85fff0f15383a104

--- backend/Animate/AnimatedDrawings/examples/image_to_animation.py
+++ backend/Animate/AnimatedDrawings/examples/image_to_animation.py
@@ -17,12 +17,11 @@ from pkg_resources import resource_filename
 
 def image_to_animation(char_anno_dir: str, motion_cfg_fn: str, retarget_cfg_fn: str, animation_type=None,
                        character=None):
-    if animation_type == 'friends':
+    if animation_type != 'animals':
         image_to_annotations(char_anno_dir, animation_type)
 
     # 동물그리기) 모델이 도출한 annotation(mask, texture)을 프리셋에 맞게 수정
     if animation_type == 'tales' or animation_type == 'animals':
-
         # 프리셋 반영 1) char_cfg파일 복사
         try:
             # Copy the file to the destination folder
