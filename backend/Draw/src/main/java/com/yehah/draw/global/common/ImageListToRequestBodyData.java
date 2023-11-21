1f62da3409a01789ad17151989976b05657ff499
deaf52 <eorms96@naver.com>
2023-11-22 01:58:48 +0900

    #S09P31D106 #done #fix(Draw) : reqeustBody 데이터 인풋 관리

--- backend/Draw/src/main/java/com/yehah/draw/global/common/ImageListToRequestBodyData.java
+++ backend/Draw/src/main/java/com/yehah/draw/global/common/ImageListToRequestBodyData.java
@@ -8,22 +8,24 @@ import java.util.Map;
 @Slf4j
 public class ImageListToRequestBodyData {
     public static void addImage(Map<String, byte[]> images, MultiValueMap<String, Object> bodyData) {
-        log.error("images 사이즈 = " + images.size());
-        if(images.size() < 2) {
-            System.out.println("images 사이즈 2보다 작음" + images.size());
-            log.error("images 사이즈 2보다 작음" + images.size());
+        log.info("images 사이즈 = " + images.size());
+
+        if(images.containsKey("output.jpg")){
+            bodyData.add("image", new ByteArrayResource(images.get("output.jpg")){
+                @Override
+                public String getFilename() throws IllegalStateException{
+                    return "image.jpg";
+                }
+            });
         }
-        bodyData.add("image_mask", new ByteArrayResource(images.get("output_mask.jpg")){
-            @Override
-            public String getFilename() throws IllegalStateException{
-                return "image_mask.jpg";
-            }
-        });
-        bodyData.add("image", new ByteArrayResource(images.get("output.jpg")){
-            @Override
-            public String getFilename() throws IllegalStateException{
-                return "image.jpg";
-            }
-        });
+
+        if(images.containsKey("output_mask.jpg")){
+            bodyData.add("image_mask", new ByteArrayResource(images.get("output_mask.jpg")){
+                @Override
+                public String getFilename() throws IllegalStateException{
+                    return "image_mask.jpg";
+                }
+            });
+        }
     }
 }
